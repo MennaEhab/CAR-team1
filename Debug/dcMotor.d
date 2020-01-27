fe5780fdab3bcace6dc17c9da99733c35e99f29f
ahmed-fathi-1992 <60051775+ahmed-fathi-1992@users.noreply.github.com>
2020-01-27 19:42:01 +0200

    the final :)

---
+++ Debug/dcMotor.d
@@ -0,0 +1,19 @@
+dcMotor.d dcMotor.o: .././dcMotor.c .././dcMotor.h .././dcMotorConfig.h \
+ .././gpio.h .././std_types.h .././registers.h .././timers.h .././led.h \
+ .././ledConfig.h
+
+.././dcMotor.h:
+
+.././dcMotorConfig.h:
+
+.././gpio.h:
+
+.././std_types.h:
+
+.././registers.h:
+
+.././timers.h:
+
+.././led.h:
+
+.././ledConfig.h:
